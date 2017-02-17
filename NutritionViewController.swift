8e314b2aefabb18dcf2ec7a32168d7837e2b619b
bboye003 <bboye003@plattsburgh.edu>
2017-02-17 11:22:41 -0500

    Implementing calculations for getting macros and outputting calories

--- NutritionViewController.swift
+++ NutritionViewController.swift
@@ -13,6 +13,26 @@ class NutritionViewController: UIViewController {
     @IBOutlet weak var carbs: UITextField!
     @IBOutlet weak var protein: UITextField!
     @IBOutlet weak var fats: UITextField!
+    
+    
+    
+    @IBAction func calculateCalories(_ sender: UIButton) {
+        let entCarbs = Int(self.carbs.text!)
+        let entProtein = Int(self.protein.text!)
+        let entFats = Int(self.fats.text!)
+        
+        //for every gram of protein and carb is 4 calories
+        let carbCal = entCarbs! * 4
+        let proCal = entProtein! * 4
+        //for ebery gram of fat is 9 calories
+        let fatCal = entFats! * 9
+        
+        let totalCal = carbCal + proCal + fatCal
+        
+        //caloriesOutput.text = String(totalCal)
+    }
+    
+    
     override func viewDidLoad() {
        
         super.viewDidLoad()
