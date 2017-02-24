41ec5f5f0aeeb0e190c250a509cc50f3da9d8593
bboye003 <bboye003@plattsburgh.edu>
2017-02-23 22:12:24 -0500

    Button connects to webpage

--- OnPoint Workouts/CrossFitViewController.swift
+++ OnPoint Workouts/CrossFitViewController.swift
@@ -12,14 +12,11 @@ class CrossFitViewController: UIViewController {
 
     @IBAction func WODlink(_ sender: UIButton) {
         if let url = NSURL(string: "https://www.crossfit.com/workout/") {
-           // UIApplication.sharedApplication.openURL(url as URL)
+            UIApplication.shared.openURL(url as URL)
         }
+    
     }
   
-    
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,15 +29,5 @@ class CrossFitViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
