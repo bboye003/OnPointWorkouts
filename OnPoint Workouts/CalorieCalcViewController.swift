7c394d03681f996a25d11b9809c5b8c3fa8a4dcd
bboye003 <bboye003@plattsburgh.edu>
2017-04-07 11:18:03 -0400

    Still seperating

---
+++ OnPoint Workouts/CalorieCalcViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CalorieCalcViewController.swift
+//  OnPoint Workouts
+//
+//  Created by Bret Boyer on 4/7/17.
+//  Copyright © 2017 OnPoint Workouts. All rights reserved.
+//
+
+import UIKit
+
+class CalorieCalcViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
