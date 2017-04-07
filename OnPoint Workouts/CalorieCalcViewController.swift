cca10f15e645a155b3c55156c2fe72bd1138fb82
bboye003 <bboye003@plattsburgh.edu>
2017-04-07 11:24:16 -0400

    moving calorie calculator

--- OnPoint Workouts/CalorieCalcViewController.swift
+++ OnPoint Workouts/CalorieCalcViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class CalorieCalcViewController: UIViewController {
 
+    @IBOutlet var carbTextField: UITextField!
+    @IBOutlet var fatTextField: UITextField!
+    @IBOutlet var proteinTextField: UITextField!
+    @IBOutlet var outputLabel: UILabel!
+    
+    @IBAction func calorieCalculator(_ sender: Any) {
+        var carb, fat, protein : Int
+        
+        carb = Int(self.carbTextField.text!)!
+        fat = Int(self.fatTextField.text!)!
+        protein = Int(self.proteinTextField.text!)!
+        
+        carb = (carb * 4)
+        fat = (fat * 9)
+        protein = (protein * 4)
+        
+        let totalCalories = (carb) + (fat) + (protein)
+        
+        return outputLabel.text = String(totalCalories)
+        
+
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
