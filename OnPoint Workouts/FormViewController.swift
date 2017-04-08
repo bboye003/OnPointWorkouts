7916cbdc405e6d89696b35a34e3fb3cd152c593d
bboye003 <bboye003@plattsburgh.edu>
2017-04-08 17:40:49 -0400

    Putting in Links for form

--- OnPoint Workouts/FormViewController.swift
+++ OnPoint Workouts/FormViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class FormViewController: UIViewController {
 
+    @IBAction func benchButton(_ sender: Any) {
+        let url = URL(string: "https://www.youtube.com/watch?v=gRVjAtPip0Y")!
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url)
+        }
+    }
+
+    @IBAction func deadliftButton(_ sender: Any) {
+        let url = URL(string: "https://www.youtube.com/watch?v=-4qRntuXBSc")!
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    @IBAction func squatButton(_ sender: Any) {
+        let url = URL(string: "https://www.youtube.com/watch?v=Dy28eq2PjcM")!
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url)
+        }
+    }
+    @IBAction func powerCleanButton(_ sender: Any) {
+        let url = URL(string: "https://www.youtube.com/watch?v=_LpPUmrKEg8")!
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
