05cebb5fc881da6a73fa7a138e4a4e30c0f742c4
bboye003 <bboye003@plattsburgh.edu>
2017-04-08 17:34:00 -0400

    Teaching Form added to UIControllers

---
+++ OnPoint Workouts/FormViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FormViewController.swift
+//  OnPoint Workouts
+//
+//  Created by Bret Boyer on 4/8/17.
+//  Copyright © 2017 OnPoint Workouts. All rights reserved.
+//
+
+import UIKit
+
+class FormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    }
