f07dd02008056a0cba6bb3e206a8bf330aa3434c
bboye003 <bboye003@plattsburgh.edu>
2017-04-18 13:09:25 -0400

    Working on Progress tab

---
+++ OnPoint Workouts/EnterWeightViewController.swift
@@ -0,0 +1,24 @@
+//
+//  EnterWeightViewController.swift
+//  OnPoint Workouts
+//
+//  Created by Bret Boyer on 4/18/17.
+//  Copyright © 2017 OnPoint Workouts. All rights reserved.
+//
+
+import UIKit
+
+class EnterWeightViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
